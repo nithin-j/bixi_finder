5712a994ea61d49a28ba008d3b416bec92494bee
Nithin Joy <56895565+nithin-j@users.noreply.github.com>
2020-05-14 11:00:43 -0400

    added map using mapbox and fixed issues with updating current location

--- BixiFinder/app/src/main/java/com/example/bixifinder/MainActivity.kt
+++ BixiFinder/app/src/main/java/com/example/bixifinder/MainActivity.kt
@@ -1,98 +1,84 @@
 package com.example.bixifinder
 
+import android.annotation.SuppressLint
 import android.graphics.Paint
 import android.graphics.RenderNode
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.LocaleList
+import android.os.PersistableBundle
 import android.widget.Toast
-import com.mapbox.android.core.location.LocationEngine
-import com.mapbox.android.core.location.LocationEngineListener
-import com.mapbox.android.core.location.LocationEnginePriority
-import com.mapbox.android.core.location.LocationEngineProvider
+import androidx.core.content.ContextCompat
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
-import com.mapbox.mapboxsdk.constants.Style
-import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
-import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin
-import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode
-import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineListener {
+class MainActivity : AppCompatActivity(), PermissionsListener, OnMapReadyCallback {
 
-    private lateinit var mapView: MapView
-    private lateinit var map: MapboxMap
-    private lateinit var userPermission: PermissionsManager
-    private lateinit var originLovation: Location
-
-    private var locationEngine: LocationEngine? = null
-    private var locationLayerPlugin: LocationLayerPlugin? = null
+    private var permissionsManager: PermissionsManager = PermissionsManager(this)
+    private lateinit var mapboxMap: MapboxMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Mapbox.getInstance(applicationContext,getString(R.string.mapbox_access_token))
         setContentView(R.layout.activity_main)
 
-        Mapbox.getInstance(applicationContext,getString(R.string.mapbox_access_token))
-        mapView = findViewById(R.id.mapView)
         mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync { mapboxMap ->
-            map = mapboxMap
-            enableLocation()
+        mapView.getMapAsync(this)
+
+        fab_dark.setOnClickListener {
+            mapboxMap.setStyle(Style.DARK)
+        }
+        fab_light.setOnClickListener {
+            mapboxMap.setStyle(Style.MAPBOX_STREETS)
         }
 
     }
 
-    private fun enableLocation() {
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        this.mapboxMap = mapboxMap
+        mapboxMap.setStyle(Style.MAPBOX_STREETS){
+            enableLocationComponent(it)
+        }
+
+
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun enableLocationComponent(loadedMapStyle: Style) {
         if (PermissionsManager.areLocationPermissionsGranted(this)){
-            initializeLocationEngine()
-            initializeLocationLayer()
+            val customLocationComponentOptions = LocationComponentOptions.builder(this)
+                .trackingGesturesManagement(true)
+                .accuracyColor(ContextCompat.getColor(this, R.color.accent))
+                .build()
+
+            val locationComponentActivationOptions = LocationComponentActivationOptions.builder(this,
+                loadedMapStyle)
+                .locationComponentOptions(customLocationComponentOptions)
+                .build()
+            mapboxMap.locationComponent.apply {
+                activateLocationComponent(locationComponentActivationOptions)
+
+                isLocationComponentEnabled = true
+
+                cameraMode = CameraMode.TRACKING
+                renderMode = RenderMode.COMPASS
+            }
         }
         else{
-            userPermission = PermissionsManager(this)
-            userPermission.requestLocationPermissions(this)
-        }
-    }
-
-    private fun initializeLocationEngine(){
-        locationEngine = LocationEngineProvider(this).obtainBestLocationEngineAvailable()
-        locationEngine?.priority = LocationEnginePriority.HIGH_ACCURACY
-        locationEngine?.activate()
-
-        val lastLocation = locationEngine?.lastLocation
-        if (lastLocation != null){
-            originLovation = lastLocation
-            setCameraPosition(lastLocation)
-        }
-        else{
-            locationEngine?.addLocationEngineListener(this)
-        }
-    }
-
-    private fun initializeLocationLayer(){
-        locationLayerPlugin = LocationLayerPlugin(mapView, map, locationEngine)
-        locationLayerPlugin?.setLocationLayerEnabled(true)
-        locationLayerPlugin?.cameraMode = CameraMode.TRACKING
-        locationLayerPlugin?.renderMode = RenderMode.NORMAL
-    }
-
-    private fun setCameraPosition(location: Location){
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(
-            LatLng(location.latitude, location.longitude), 17.0))
-    }
-
-    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
-        //show toast.
-    }
-
-    override fun onPermissionResult(granted: Boolean) {
-        if (granted){
-            enableLocation()
+            permissionsManager = PermissionsManager(this)
+            permissionsManager.requestLocationPermissions(this)
         }
     }
 
@@ -101,23 +87,21 @@ class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineLis
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        userPermission.onRequestPermissionsResult(requestCode,permissions,grantResults)
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
-    override fun onLocationChanged(location: Location?) {
-        location?.let {
-            originLovation = location
-            setCameraPosition(location)
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted){
+            enableLocationComponent(mapboxMap.style!!)
+        }else{
+            Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show()
+            finish()
         }
-    }
-
-    override fun onConnected() {
-        locationEngine?.requestLocationUpdates()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mapView.onResume()
     }
 
     override fun onStart() {
@@ -125,9 +109,9 @@ class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineLis
         mapView.onStart()
     }
 
-    override fun onStop() {
-        super.onStop()
-        mapView.onStop()
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
     }
 
     override fun onPause() {
@@ -135,14 +119,9 @@ class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineLis
         mapView.onPause()
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView.onSaveInstanceState(outState)
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView.onLowMemory()
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
     }
 
     override fun onDestroy() {
@@ -150,7 +129,15 @@ class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineLis
         mapView.onDestroy()
     }
 
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
 
 
 }
